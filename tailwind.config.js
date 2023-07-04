5b416cf9e8b179c1a8cca3dc246ef2763b6b378c
Marcelo Alarcón <marceloalarconbarrenechea@gmail.com>
2023-07-04 16:20:30 -0400

    Add navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    fontFamily: {
+      monument: "Monument",
+    },
+    colors: {
+      "primary-color": "#5f43b2",
+      "secondary-color": "#3a3153",
+    },
   },
   plugins: [],
 };
